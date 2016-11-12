9fe6dd2c6126a165b60fa71473696fd4cba74411
Kelsea <andersonkelsea@gmail.com>
2016-11-11 22:08:23 -0500

    Adds resume page, move to top button

--- js/app.js
+++ js/app.js
@@ -22,4 +22,22 @@ $(document).ready(function(){
       });
     } // End if
   });
+
+  //Back to top of page 
+  var offset = 250; 
+  var duration = 600; 
+  $(window).scroll(function() { 
+    if($(this).scrollTop() > offset) { 
+      $('.back-to-top').fadeIn(duration); 
+    } else { 
+      $('.back-to-top').fadeOut(duration); 
+    } 
+  }); 
+
+  $('.back-to-top').click(function(event) { 
+      event.preventDefault(); 
+      $('html, body').animate({scrollTop: 0}, duration); 
+      return false;
+  })
+
 });
